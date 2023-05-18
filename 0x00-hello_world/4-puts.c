2c19a70a1fd74de9d0de96597a30b9f21c581c39
Seqaqa Rampeta <ramokhethirampeta2@gmail.com>
2023-05-18 12:28:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main -prints a program that prints a line with puts function
  * Return: Always 0
  */
-
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+ puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }
