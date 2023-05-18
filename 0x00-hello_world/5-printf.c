41b7526d5cff27f97b8bd77662bafa8e83c28b23
Seqaqa Rampeta <ramokhethirampeta2@gmail.com>
2023-05-18 16:00:45 -0700

    100-intel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -with proper grammar, but the outcome is a piece of art
+ * main - A program that prints a line using printf function
  *Return : 0
  */
 int main(void)
 {
-printf("with proper grammar,but the outcome is a piece of art\n");
-*Return : (0);
+printf("with proper grammar,but the outcome is a piece of art\n")
+Return : (0);
 }
 
