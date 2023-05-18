f00c2d3d1e6020b7432551298e91dd00f93c84ac
Seqaqa Rampeta <ramokhethirampeta2@gmail.com>
2023-05-18 12:25:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main -prints a program that prints a line with puts function
+ * Return: Always 0
+ */
 
-int main() {
-  puts("Programming is like building a multilingual puzzle");
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }
